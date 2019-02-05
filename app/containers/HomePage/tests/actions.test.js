97fde1fd689bb7b3851ea44898649e0283875777
Bernard Kim <bernardkim.seattle@gmail.com>
2019-02-05 15:14:32 -0800

    homepage testing

--- app/containers/HomePage/tests/actions.test.js
+++ app/containers/HomePage/tests/actions.test.js
@@ -1,13 +1,43 @@
-import { defaultAction } from '../actions';
-import { DEFAULT_ACTION } from '../constants';
+import {
+  loadArticlesAll,
+  loadArticlesAllError,
+  loadArticlesAllSuccess,
+} from '../actions';
+import {
+  LOAD_ARTICLES_ALL,
+  LOAD_ARTICLES_ALL_ERROR,
+  LOAD_ARTICLES_ALL_SUCCESS,
+} from '../constants';
 
-describe.skip('HomePage actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
+describe('HomePage actions', () => {
+  describe('Load Articles All Action', () => {
+    it('has a type of LOAD_ARTICLES_ALL', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: LOAD_ARTICLES_ALL,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(loadArticlesAll()).toEqual(expected);
+    });
+  });
+
+  describe('Load Articles All Success Action', () => {
+    it('has a type of LOAD_ARTICLES_ALL_SUCCESS', () => {
+      const articles = [];
+      const expected = {
+        type: LOAD_ARTICLES_ALL_SUCCESS,
+        articles,
+      };
+      expect(loadArticlesAllSuccess(articles)).toEqual(expected);
+    });
+  });
+
+  describe('Load Articles All Error Action', () => {
+    it('has a type of LOAD_ARTICLES_ALL_ERROR', () => {
+      const error = new Error('Test Error');
+      const expected = {
+        type: LOAD_ARTICLES_ALL_ERROR,
+        error,
+      };
+      expect(loadArticlesAllError(error)).toEqual(expected);
     });
   });
 });
--- app/containers/HomePage/tests/reducer.test.js
+++ app/containers/HomePage/tests/reducer.test.js
@@ -1,8 +1,36 @@
-import { fromJS } from 'immutable';
-import homePageReducer from '../reducer';
+// import { fromJS } from 'immutable';
+import homePageReducer, { initialState } from '../reducer';
+import {
+  loadArticlesAll,
+  loadArticlesAllError,
+  loadArticlesAllSuccess,
+} from '../actions';
 
-describe.skip('homePageReducer', () => {
+describe('homePageReducer', () => {
+  let state;
+  beforeEach(() => {
+    state = initialState;
+  });
+
   it('returns the initial state', () => {
-    expect(homePageReducer(undefined, {})).toEqual(fromJS({}));
+    expect(homePageReducer(state, {})).toMatchSnapshot();
+  });
+
+  it('handle the load articles all action', () => {
+    expect(homePageReducer(state, loadArticlesAll())).toMatchSnapshot();
+  });
+
+  it('handle the load articles all success action', () => {
+    const articles = [];
+    expect(
+      homePageReducer(state, loadArticlesAllSuccess(articles)),
+    ).toMatchSnapshot();
+  });
+
+  it('handle the load articles all error action', () => {
+    const error = new Error('Test Error');
+    expect(
+      homePageReducer(state, loadArticlesAllError(error)),
+    ).toMatchSnapshot();
   });
 });
